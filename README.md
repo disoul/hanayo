5e3d25dbf0c5074b9a4297c252ce113e9301303c
disoul <starky_ds@sina.cn>
2015-11-16 00:57:22 +0800

    rename bond to hanayo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# 开了一波坑
-希望这辈子能填完:P
+# Hanayo
+基于node的一个静态博客站点生成器
+
+# Usage
+
+## Install
+
+```shell
+git clone https://github.com/disoul/hanayo && cd hanayo
+npm install hanayo -g
+npm install
+```
+## Start
+start server
+
+```shell
+hanayo -s start
+```
--- hanayo/server/server.js
+++ hanayo/server/server.js
@@ -24,5 +24,6 @@ app.get('/', function(req, res) {
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Bond app listening at http://%s:%s', host, port);
+    console.log('Hanayo listening at http://%s:%s', host, port);
+    console.log('Dare ga tasukete~');
 });

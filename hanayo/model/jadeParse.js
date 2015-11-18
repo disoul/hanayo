49464612e31c81a019b100be3558240279061505
disoul <starky_ds@sina.cn>
2015-11-18 16:03:34 +0800

    修改单article时的bug

--- hanayo/model/jadeParse.js
+++ hanayo/model/jadeParse.js
@@ -13,7 +13,7 @@ function JadeParse(opt) {
   Duplex.call(this, {objectMode: true});
   var self = this;
   this.obj = {};
-  this.articles = {};
+  this.articles = {articles: []};
   this.mergeObj = function(obj) {
     for (var attr in obj) {
       self.obj[attr] = obj[attr];
@@ -39,15 +39,7 @@ function JadeParse(opt) {
 JadeParse.prototype._write = function(chunk, encode, callback) {
   var chunkObj = JSON.parse(chunk.toString());
   if ((chunkObj.flag == 'article') && (this.isMerge(chunkObj))) {
-    if (Object.keys(this.articles).length === 0) {
-      this.articles = { article: chunkObj };
-    }else if (this.articles.article !== undefined ){
-      this.articles = { articles: [this.articles.article]};
-      this.articles.article = undefined;
-      this.articles.articles.push(chunkObj);
-    }else {
-      this.articles.articles.push(chunkObj);
-    }
+    this.articles.articles.push(chunkObj);
     this.mergeObj(this.articles);
   }else if (chunkObj.flag != 'article'){
     this.mergeObj(chunkObj);
---
+++ views/article/helloworld.md
@@ -0,0 +1,8 @@
+title: HelloWorld
+tag: test test2
+author: DiSoul
+------------------------
+# HelloWorld
+markdown test
+* Test
+* Test

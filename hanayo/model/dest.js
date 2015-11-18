9bfe253c3598501f3ee7f6314cf12174e56114f9
disoul <starky_ds@sina.cn>
2015-11-18 15:39:28 +0800

    add archive index

--- hanayo/model/dest.js
+++ hanayo/model/dest.js
@@ -14,28 +14,42 @@ function DestStream(opt) {
   Writable.call(this, {objectMode: true});
   var self = this;
   this.obj = {};
+  this.archiveListObj = { dateList: [] };
   this.archivePath = path.resolve(
     __dirname, '../../views/archives');
   this.articlePath = path.resolve(__dirname, '../../views/article');
   this.jadePath = path.resolve(__dirname, '../../views/template/default/pages');
 
   this.getArticleObj = function(globalObj, articleObj){
-    globalObj.articles = undefined;
     globalObj.article = articleObj;
     return globalObj;
+  };
+
+  this.getListObj = function(globalObj, list) {
+    globalObj.list = list;
+    return globalObj;
+  };
+
+  this.pushListObj = function(ele) {
+    for (var i in this.archiveListObj) {
+      if (this.archiveListObj[i] == ele)
+        return;
+    } 
+    this.archiveListObj.dateList.push(ele);
   };
 }
 
 DestStream.prototype._write = function(chunk, encoding, callback) {
   this.obj = JSON.parse(chunk);
   this.archive_article(this.obj); // write archives articles
-  console.log(this.obj);
+//  console.log(this.obj);
   
 };
 
+
 DestStream.prototype.archive_article = function(obj) {
   var self = this;
-  obj.articles.map(function(article) {
+  obj.articles.map(function(article, index, articles) {
     var jadefn = jade.compileFile(
       path.join(self.jadePath, 'article.jade'),
       { cache: true }
@@ -44,16 +58,50 @@ DestStream.prototype.archive_article = function(obj) {
       self.archivePath, article.time.year, article.time.month);
     mkdirp(archive_path, function(err) {
         if (err) throw err;
+        self.pushListObj(
+          article.time.year + '/' + article.time.month);
         fs.writeFile(
           path.join(archive_path, article.name + '.html'),
           jadefn(self.getArticleObj(obj, article)),
           function (err) {
             if (err) throw err;
             console.log('write file', article.name);
+
+            if (index == articles.length - 1) {
+              self.archive_list();
+            }
           }
         );
       }
     );
+  });
+};
+
+DestStream.prototype.archive_list = function() {
+  var self = this;
+  var archiveObj =  this.getListObj(
+    this.obj, 
+    this.archiveListObj.dateList.map(function(ele) {
+      return {name: ele, link: 'archives/' + ele + '/' + 'index.html'};
+    }) 
+  );
+
+  console.log(archiveObj);
+  var jadefn = jade.compileFile(
+    path.join(self.jadePath, 'archive.jade'),
+    { cache: true }
+  );
+  fs.writeFile(
+    path.join(self.archivePath, 'index.html'),
+    jadefn(archiveObj),
+    function(err) {
+      if (err) throw err;
+      console.log('write archive');
+    }
+  );
+
+  this.archiveListObj.dateList.map(function(date) {
+    
   });
 };
 

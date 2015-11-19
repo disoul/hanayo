ad52585eff8a8840568a6fcc3916dfb45052e537
disoul <starky_ds@sina.cn>
2015-11-19 17:03:38 +0800

    add hanayo init

--- hanayo/cli/hanayo-cli.js
+++ hanayo/cli/hanayo-cli.js
@@ -4,6 +4,8 @@ var program = require('commander');
 var compile = new require('../util/comileJade.js')();
 var Childprocess = require('child_process');
 var path = require('path');
+var ncp = require('ncp');
+var fs = require('fs');
 var cmdValue,envValue = '';
 program
   .version('0.0.1')
@@ -50,7 +52,29 @@ function server() {
 }
 
 function init() {
-  console.log('filename', __filename);
-  console.log('dirname', __dirname);
-  console.log('cwd', process.cwd());
+  console.log('copy files...');
+  ncp.limit = 16;
+  ncp(
+    path.resolve(__dirname, '../../blog.yml'), 
+    path.resolve(process.cwd(), './blog.yml'));
+  ncp(
+    path.resolve(__dirname, '../../article'), 
+    path.resolve(process.cwd(), './article'));
+  
+  ncp(
+    path.resolve(__dirname, '../../views'), 
+    path.resolve(process.cwd(), './views'),
+    function(err) {
+      if (err) throw err;
+      console.log('copy complete!');
+      console.log('bower install...');
+      Childprocess.exec('bower install', {
+        cwd: path.resolve(process.cwd(), './views/template/default/public')},
+        function(error, stdout, stderr) {
+          if (error !== null) {
+            console.error('error:', error);
+          }
+      }).stdout.pipe(process.stdout);
+    }
+  );
 }
